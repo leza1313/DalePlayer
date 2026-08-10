32fb25fa9708de297f5f6cca2922f4bb8d5749d6
leza1313 <leza1313@gmail.com>
2026-08-10 14:37:12 +0200

    Cambio contraseña

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 // Hash of the default password ("dalePlayer2026")
 // Change this value to change the app password.
 // Compute new hash with the function in auth.ts hashPassword()
-export const APP_PASSWORD_HASH = 'aa1fbdc0'
+export const APP_PASSWORD_HASH = 'b9368134'
